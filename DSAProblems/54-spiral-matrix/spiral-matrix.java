356945722473b47d68043f9e7006ea1a9392e01c
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-05-12 20:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (97.14%) - LeetSync

--- DSAProblems/54-spiral-matrix/spiral-matrix.java
+++ DSAProblems/54-spiral-matrix/spiral-matrix.java
@@ -1,57 +1,46 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
-        int row = matrix.length;
-        int col = matrix[0].length;
-
+        int N = matrix.length;
+        int M = matrix[0].length;
+        List<Integer> list = new ArrayList<>();
         int i = 0;
         int j = 0;
-        int rsteps = row - 1;
-        int csteps = col - 1;
-        int count = 0;
-
-    while(rsteps >= 1 && csteps >= 1){
-        for(int k = 0;k < csteps;k++){
-            ans.add(matrix[i][j]);
-            j++;
-            count++;
-        }
-
-        for(int k = 0;k < rsteps;k++){
-            ans.add(matrix[i][j]);
+        int rsteps = N-1;
+        int csteps = M-1;
+        while(rsteps>=1 && csteps>=1){
+            for(int k=0;k<csteps;k++){
+                list.add(matrix[i][j]);
+                j++;
+            }
+            for(int k=0;k<rsteps;k++){
+                list.add(matrix[i][j]);
+                i++;
+            }
+            for(int k=0;k<csteps;k++){
+                list.add(matrix[i][j]);
+                j--;
+            }
+            for(int k=0;k<rsteps;k++){
+                list.add(matrix[i][j]);
+                i--;
+            }
             i++;
-            count++;
-        }
-
-        for(int k = csteps;k >= 1;k--){
-            ans.add(matrix[i][j]);
-            j--;
-            count++;
-        }
-
-        for(int k = rsteps;k >= 1;k--){
-            ans.add(matrix[i][j]);
-            i--;
-            count++;
-        }
-
-        rsteps = rsteps - 2;
-        csteps = csteps - 2;
-        i++;
-        j++;
-    }
-
-    if(rsteps == 0){
-        for(int k = 0;k < csteps + 1;k++){
-            ans.add(matrix[i][j]);
             j++;
+            rsteps = rsteps - 2;
+            csteps = csteps - 2;
         }
-    }else if(csteps == 0){
-        for(int k = 0;k < rsteps + 1;k++){
-            ans.add(matrix[i][j]);
-            i++;
+        if(rsteps==0){
+            for(int k=0;k<csteps+1;k++){
+                list.add(matrix[i][j]);
+                j++;
+            }
         }
-    }
-        return ans;
+        else if(csteps==0){
+            for(int k=0;k<rsteps+1;k++){
+                list.add(matrix[i][j]);
+                i++;
+            }
+        }
+        return list;
     }
 }
