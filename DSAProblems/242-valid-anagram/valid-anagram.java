65230d8de94380f669064474a1d81aa04dd4632a
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-05-18 18:08:35 +0530

    Time: 2 ms (99.62%) | Memory: 44.5 MB (60.40%) - LeetSync

---
+++ DSAProblems/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int count[] = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(char c: s.toCharArray()){
+            count[c - 'a']++;
+        }
+        for(char c: t.toCharArray()){
+            count[c - 'a']--;
+        }
+        for(int i : count){
+            if(i != 0) return false;
+        }
+        return true;
+    }
+}
