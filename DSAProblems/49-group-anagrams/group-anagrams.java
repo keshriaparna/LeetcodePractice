8f4daa975bac415f9d19226a88110c8ff9efc132
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-05-26 09:04:41 +0530

    Time: 20 ms (14.11%) | Memory: 50.7 MB (12.94%) - LeetSync

--- DSAProblems/49-group-anagrams/group-anagrams.java
+++ DSAProblems/49-group-anagrams/group-anagrams.java
@@ -1,18 +1,38 @@
+// Brute Force - T.C: O(n*mlogm), S.C: O(n*m)
+//class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         HashMap<String,List<String>> groups = new HashMap<>();
+//         for(String word: strs){
+//             char[] sortedWord = word.toCharArray();
+//             Arrays.sort(sortedWord);
+//             String key = new String(sortedWord);
+
+//             groups.putIfAbsent(key, new ArrayList<>());
+//             groups.get(key).add(word);
+//         }
+//         List<List<String>> result = new ArrayList<>();
+//         for(List<String> group : groups.values()){
+//             result.add(group);
+//         }
+//         return result;
+//     }
+// }
+// Optimal - T.C: O(n*m), S.C: O(n*m)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> groups = new HashMap<>();
-        for(String word: strs){
-            char[] sortedWord = word.toCharArray();
-            Arrays.sort(sortedWord);
-            String key = new String(sortedWord);
-
-            groups.putIfAbsent(key, new ArrayList<>());
-            groups.get(key).add(word);
+        for(String word : strs){
+            int[] freq = new int[26];
+            for(char c : word.toCharArray()){
+                freq[c-'a']++;
+            }
+            StringBuilder key = new StringBuilder();
+            for(int count : freq){
+                key.append('#').append(count);
+            }
+            groups.putIfAbsent(key.toString(),new ArrayList<>());
+            groups.get(key.toString()).add(word);
         }
-        List<List<String>> result = new ArrayList<>();
-        for(List<String> group : groups.values()){
-            result.add(group);
-        }
-        return result;
+        return new ArrayList<>(groups.values());
     }
 }
