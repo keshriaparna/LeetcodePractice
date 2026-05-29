be40fc5911f2f5208b81d0bb8fa31d1cc8768e03
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-05-29 09:34:40 +0530

    Time: 2 ms (97.85%) | Memory: 45.1 MB (86.70%) - LeetSync

--- DSAProblems/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ DSAProblems/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,16 +1,37 @@
+// Brute Force - T.C: O(N^2)-> iterating array twice using 2 loops, S.C: O(256) - hash array
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxLen = 0; // initialized as zero because string can be empty in that case length will be zero so it will return maxLength as zero.
+//         for(int i=0;i<n;i++){
+//             int[] hash = new int[256];
+//             Arrays.fill(hash,0);
+//             for(int j=i;j<n;j++){
+//                 if(hash[s.charAt(j)] == 1) break;
+//                 hash[s.charAt(j)] = 1;
+//                 int len = j-i+1;
+//                 maxLen = Math.max(maxLen, len);
+//             }
+//         }
+//         return maxLen;
+//     }
+// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        int maxLen = 0; // initialized as zero because string can be empty in that case length will be zero so it will return maxLength as zero.
-        for(int i=0;i<n;i++){
-            int[] hash = new int[256];
-            Arrays.fill(hash,0);
-            for(int j=i;j<n;j++){
-                if(hash[s.charAt(j)] == 1) break;
-                hash[s.charAt(j)] = 1;
-                int len = j-i+1;
-                maxLen = Math.max(maxLen, len);
-            }
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        int[] hash = new int[256];
+        Arrays.fill(hash,-1);
+        while(r<n){
+            if(hash[s.charAt(r)] != -1){
+                l = Math.max(hash[s.charAt(r)] + 1, l);
+            } 
+            int len = r - l + 1;
+            maxLen = Math.max(len,maxLen);
+            hash[s.charAt(r)] = r;
+            r++;
         }
         return maxLen;
     }
