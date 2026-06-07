223e35bfb4f904da76cbc7a30fa70d4d0a3fc64a
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-07 17:49:38 +0530

    Time: 12 ms (46.26%) | Memory: 46.1 MB (64.13%) - LeetSync

--- DSAProblems/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ DSAProblems/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,3 +1,25 @@
+//Better - T.C:O(N+N)*26 , S.C:O(26)
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+//         int l = 0;
+//         int r = 0;
+//         int maxFreq = 0;
+//         int maxLen = 0;
+//         int[] freqChar = new int[26];
+//         while(r<s.length()){
+//             freqChar[s.charAt(r) - 'A']++;
+//             maxFreq = Math.max(maxFreq,freqChar[s.charAt(r) - 'A']);
+//             while((r-l+1) - maxFreq > k){
+//                 freqChar[s.charAt(l) - 'A']--;
+//                 l++;
+//             }
+//             maxLen = Math.max(maxLen,r - l + 1);
+//             r++;
+//         }
+//         return maxLen;
+//     }
+// }
+//Optimal - T.C:O(N), S.C:O(26) 
 class Solution {
     public int characterReplacement(String s, int k) {
         int l = 0;
@@ -8,7 +30,7 @@ class Solution {
         while(r<s.length()){
             freqChar[s.charAt(r) - 'A']++;
             maxFreq = Math.max(maxFreq,freqChar[s.charAt(r) - 'A']);
-            while((r-l+1) - maxFreq > k){
+            if((r-l+1) - maxFreq > k){
                 freqChar[s.charAt(l) - 'A']--;
                 l++;
             }
