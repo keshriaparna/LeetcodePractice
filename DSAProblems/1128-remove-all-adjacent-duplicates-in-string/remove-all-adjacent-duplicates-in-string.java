7d086cdb51f3a4cff74047ce4b8034b2c1788013
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-09 18:01:01 +0530

    Time: 11 ms (88.40%) | Memory: 47.3 MB (39.42%) - LeetSync

--- DSAProblems/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ DSAProblems/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,18 +1,34 @@
+//Using stack and string builder
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             if(st.isEmpty() || ch != st.peek()){
+//                 st.push(ch);
+//             }
+//             else{
+//                 st.pop();
+//             }
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         for(char ch : st) {
+//             sb.append(ch);
+//         }
+//         return sb.toString();
+//     }
+// }
+//Using StringBuilder
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(st.isEmpty() || ch != st.peek()){
-                st.push(ch);
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()) {
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == ch){
+                sb.deleteCharAt(sb.length() - 1);
             }
             else{
-                st.pop();
+                sb.append(ch);
             }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(char ch : st) {
-            sb.append(ch);
         }
         return sb.toString();
     }
