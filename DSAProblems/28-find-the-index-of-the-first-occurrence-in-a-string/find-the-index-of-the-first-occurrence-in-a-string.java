d0a41460e3e8fdeefd33b3025356f5ab248df797
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-10 14:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (18.71%) - LeetSync

--- DSAProblems/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ DSAProblems/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,26 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         int n = haystack.length();
+//         int m = needle.length();
+//         for(int i=0;i<n-m+1;i++){
+//             int temp = i;
+//             int j=0;
+//             for(j=0;j<m;j++){
+//                 if(haystack.charAt(temp)!= needle.charAt(j)){
+//                     break;
+//                 }
+//                 temp++;
+//             }
+//             if(j == m){
+//             return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+//Optimal Approach - using indexOf method
 class Solution {
     public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        for(int i=0;i<n-m+1;i++){
-            int temp = i;
-            int j=0;
-            for(j=0;j<m;j++){
-                if(haystack.charAt(temp)!= needle.charAt(j)){
-                    break;
-                }
-                temp++;
-            }
-            if(j == m){
-            return i;
-            }
-        }
-        return -1;
+        return haystack.indexOf(needle);
     }
 }
