a623145a8653d40bd35abe06ad1e79aecedc0f81
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-13 12:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (65.61%) - LeetSync

--- DSAProblems/35-search-insert-position/search-insert-position.java
+++ DSAProblems/35-search-insert-position/search-insert-position.java
@@ -11,7 +11,7 @@ class Solution {
             else if(nums[mid]>target){
                 high = mid - 1;
             }
-            else {
+            else{
                 low = mid + 1;
             }
         }
