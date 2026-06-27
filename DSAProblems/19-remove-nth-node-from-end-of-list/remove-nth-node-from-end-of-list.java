6b4b24c6be881fab1724c8f7dc2aa58c0a70b34a
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-27 08:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (6.57%) - LeetSync

--- DSAProblems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ DSAProblems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -9,28 +9,47 @@
  * }
  */
 //Brute Force - First we are calculating the length then we are iterating and finding a before node and change it's reference of next to next.next
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         int count = 0;
+//         ListNode temp = head;
+//         while(temp != null){
+//             count++;
+//             temp = temp.next;
+//         }
+//         if(count == n){
+//             ListNode newHead = head.next;
+//             return newHead;
+//         }
+//         int res = count - n;
+//         temp = head;
+//         while(temp != null){
+//             res--;
+//             if(res == 0){
+//                 break;
+//             }
+//             temp = temp.next;
+//         }
+//         temp.next = temp.next.next;
+//         return head;
+//     }
+// }
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int count = 0;
-        ListNode temp = head;
-        while(temp != null){
-            count++;
-            temp = temp.next;
+        ListNode fastp = head;
+        ListNode slowp = head;
+        for(int i=0;i<n;i++){
+            fastp = fastp.next;
         }
-        if(count == n){
+        if(fastp == null){
             ListNode newHead = head.next;
             return newHead;
         }
-        int res = count - n;
-        temp = head;
-        while(temp != null){
-            res--;
-            if(res == 0){
-                break;
-            }
-            temp = temp.next;
+        while(fastp.next != null){
+            fastp = fastp.next;
+            slowp = slowp.next;
         }
-        temp.next = temp.next.next;
+        slowp.next = slowp.next.next;
         return head;
     }
 }
