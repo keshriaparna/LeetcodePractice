a41c21025ad059ff78d2bdf1ccd1030a94c9fd62
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-06-29 09:19:22 +0530

    Time: 8 ms (9.54%) | Memory: 46.6 MB (86.06%) - LeetSync

--- DSAProblems/141-linked-list-cycle/linked-list-cycle.java
+++ DSAProblems/141-linked-list-cycle/linked-list-cycle.java
@@ -9,15 +9,31 @@
  *     }
  * }
  */
-class Solution {
+ //Brute Force using hashing. T.C: O(N),S.C: O(N)
+ class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!= null && fast.next!= null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast) return true;
+        ListNode temp = head;
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        while(temp != null){
+            if(map.containsKey(temp)){
+            return true;
+            }
+            map.put(temp,1);
+            temp = temp.next;
         }
         return false;
     }
 }
+//Optimal using slow and fast pointer. T.C: O(N),S.C: O(1)
+// class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast!= null && fast.next!= null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(slow == fast) return true;
+//         }
+//         return false;
+//     }
+// }
