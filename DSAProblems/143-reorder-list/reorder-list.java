738b3d1f92fdafac907c3a3bbf4a24d6f7820515
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-07-02 06:10:07 +0530

    Time: 1 ms (99.93%) | Memory: 49.1 MB (71.37%) - LeetSync

---
+++ DSAProblems/143-reorder-list/reorder-list.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return;
+        //Finding the mid point of the LinkedList
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //Reversing the second half of the Linked List
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        slow.next = null; //disconnecting first and second halves
+        ListNode first = head, second = prev;
+        while(second != null){
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+
+            first.next = second;
+            second.next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
