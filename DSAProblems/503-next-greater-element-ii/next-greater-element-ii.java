c4628b4c5d59e2736efd71335c24141a10e81738
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-07-24 08:31:44 +0530

    Time: 15 ms (61.74%) | Memory: 48.1 MB (83.09%) - LeetSync

--- DSAProblems/503-next-greater-element-ii/next-greater-element-ii.java
+++ DSAProblems/503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,17 +1,39 @@
+// Brute Force - Using Circular Array Approach, T.C: O(N^2) , S.C: O(N) - ans array(outtput array)
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n = nums.length;
+//         int[] ans = new int[n];
+//         Arrays.fill(ans,-1);
+//         for(int i = 0; i < n ; i++){
+//             int currEle = nums[i];
+//             for(int j = 0; j < n; j++){
+//                 int ind = (i+j) % n;
+//                 if(nums[ind] > currEle){
+//                     ans[i] = nums[ind];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+// Optimal Approach - Doubling the array hypothetically(using modulo operator to get actual index value) and using monotonic stack, maintaining the decreasing order. T.C: O(4N) which is equivalent to O(N), S.C: O(2N) + O(N) -> 2N for stack, in worst case we will store 2N elements and pop 2N elements from the stack and O(N) for answer array.
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        Arrays.fill(ans,-1);
-        for(int i = 0; i < n ; i++){
-            int currEle = nums[i];
-            for(int j = 0; j < n; j++){
-                int ind = (i+j) % n;
-                if(nums[ind] > currEle){
-                    ans[i] = nums[ind];
-                    break;
-                }
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i= 2*n - 1; i >= 0; i--){
+            int ind = i % n;
+            int currEle = nums[ind];
+            while(!st.isEmpty() && st.peek()<= currEle){
+                st.pop();
             }
+            if(i<n){
+                ans[i] = st.isEmpty()? -1 : st.peek();
+            }
+            st.push(currEle);
         }
         return ans;
     }
