babd7a15e5421d8fee080096fe1d547f90f3efa6
keshriaparna <85886172+keshriaparna@users.noreply.github.com>
2026-09-09 23:37:12 +0530

    Time: 4 ms (95.57%) | Memory: 44.1 MB (99.39%) - LeetSync

---
+++ DSAProblems/71-simplify-path/simplify-path.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack();
+        for(String i: arr){
+            if(i.equals("..") && !stack.empty()){
+                stack.pop();
+            }
+            else if(!i.equals("") && !i.equals(".") && !i.equals("..")){
+                stack.push(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String i: stack){
+            sb.append("/");
+            sb.append(i);
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
